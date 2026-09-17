7f895583967b60b030fc228f41c63a42adf0bfbd
Kristine0901 <0901ksol0901@gmail.com>
2026-09-17 09:21:03 +0200

    Oppgave 6

--- oving4.py
+++ oving4.py
@@ -16,3 +16,22 @@ print(df.head())
 
 # Oppgave 5
 print(df.index[0])
+
+#oppgave 6
+df6 = df.loc["2026-01-01 03:00:00 +01:00"]
+df6.plot(kind= "bar", figsize=(10,5))
+plt.title("Produksjon og consumption 01.01.2026 kl 03:00:00")
+plt.grid(True)
+
+plt.savefig("Oving4/Observasjonkl0300.png")
+
+plt.show()
+
+df61 = df.loc["2026-01-01 00:00:00 +01:00": "2026-01-01 23:00:00 +01:00"]
+df61.plot(figsize=(10,5))
+plt.title("Produksjon og consumption 01.01.2026 kl 00:00 til kl 23:00 +01:00")
+plt.grid(True)
+
+plt.savefig("Oving4/ObservasjonEnLastProfil.png")
+
+plt.show()
